fc2c8a4d81d03f1c0728e520fd640f1843e6ed2a
Kaare Hoff Skovgaard <kaare@kaareskovgaard.net>
2025-02-01 23:28:15 +0100

    Make basic usage work

--- flake.nix
+++ flake.nix
@@ -1,16 +1,16 @@
 {
   description = "Flake used to define terraform helper binary that can read variables from bitwarden";
-  inputs = { };
   outputs =
     { ... }:
     let
       assertMsg = pred: msg: pred || builtins.throw msg;
       concatLines = builtins.concatStringsSep "\n";
-    in
-    {
-      overlays.default = final: prev: {
+      overlay = final: prev: {
         mkBwTerraform =
-          { items }:
+          {
+            items,
+            package ? final.terraform,
+          }:
           let
             itemNames = builtins.attrNames items;
             exports = builtins.map (
@@ -30,7 +30,8 @@
                   in
                   assert assertMsg (builtins.match "^[a-zA-Z0-9_]+$" != null) "Invalid variable name: ${varName}";
                   ''
-                    export ${varName}="$(${script} <<< "$item")";
+                    ${varName}="$(${script} <<< "$item")"
+                    export ${varName}
                   ''
                 ) varNames;
               in
@@ -40,26 +41,29 @@
               ''
             ) itemNames;
           in
-          prev.writeShellApplication {
+          final.writeShellApplication {
             name = "bw-terraform";
             runtimeInputs = [
-              prev.jq
-              prev.terraform
-              prev.bitwarden-cli
+              final.jq
+              package
+              (final.callPackage ./bitwarden-cli.nix {})
             ];
             text = ''
-              if [ -z "$BW_SESSION" ]; then
+              if [ -z "''${BW_SESSION+x}" ]; then
               	BW_SESSION="$(bw unlock --raw)"
               	echo "Bitwarden session (export as BW_SESSION to avoid repeating entries): $BW_SESSION"
               	export BW_SESSION
               fi
 
-              JQ_FIELD_SCRIPT='.fields | map(select(.name == $name))[0].value'
+              JQ_FIELD_SCRIPT='.fields | map(select(.name == $'"name))[0].value"
               ${concatLines exports}
 
               terraform "$@"
             '';
           };
       };
+    in
+    {
+      overlays.default = overlay;
     };
 }
